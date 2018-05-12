4cdff0fa93715f23e8d69e6081695daac524fb0c
Sean Rice <sean.rice@infusionsoft.com>
2018-05-12 14:38:01 -0700

    Adding infusionsoft API stuff

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //Service stuff goes here!
+var		env				= process.env.NODE_ENV || "development",
+    config			= require('./config')[env],
+    http			= require('http'),
+    compression 	= require('compression'),
+    cookieSession	= require('cookie-session'),
+    bodyParser		= require('body-parser'),
+    passport		= require('passport'),
+    express			= require('express');
+
+require('./auth/infusionsoft');
+
 const dotenv = require('dotenv');
 const fs = require('fs');
 const fastEquals = require('fast-equals');
+
 dotenv.config();
 
 const InfusionsoftRestApi = require('infusionsoft_rest_api');
@@ -202,3 +214,26 @@ try {
   console.error(err);
 }
 */
+
+
+var app = express();
+app.use(compression());
+app.use(cookieSession({ keys: config.cookieSessionKeys }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', express.static(__dirname + '/static'));
+
+app.use('/api', require('./routes'));
+
+app.get('/auth/infusionsoft', passport.authenticate('Infusionsoft', { scope: ['full'] }));
+app.get('/auth/infusionsoft/callback', passport.authenticate('Infusionsoft', { failureRedirect: '/' }),
+    function(req, res){
+        res.redirect('/');
+    }
+);
+
+http.createServer(app).listen(config.port);
+console.log("Server starting on "+config.port);
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    passport = require('passport'),
+    router  = express.Router(),
+    contactService = require('../contactService');
+
+router.use(function(req, res, next){
+    res.setHeader('Content-Type', 'application/json');
+    next();
+});
+
+router.get('/contacts',function(req,res){
+
+});
+
+module.exports = router;
